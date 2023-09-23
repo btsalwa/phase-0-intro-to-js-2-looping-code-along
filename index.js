6bebf5f67d38fb97e2e252d51ff6209d5e9aa55a
Brian Tsalwa <brian.tsalwa@moringaschool.com>
2023-09-23 17:50:25 +0300

    Finished Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const msgs = [];
+function writeCards(guests,bevent){
+    for ( let i = 0; i < guests.length; i++){
+        const msg = `Thank you, ${guests[i]}, for the wonderful ${bevent} gift!`;
+        msgs.push(msg);
+
+        if(i == guests.length-1){
+            console.log(msgs);
+            return msgs;
+        }
+    }
+}
+
+
+function countDown(){
+    let i = 10;
+    while ( i > -1){
+        console.log(i);
+        i--;
+    }
+    return i
+}
+
+countDown(); 
